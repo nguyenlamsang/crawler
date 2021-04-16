0ab1539467f9712584990abbd686d73cdcc93895
NguyenLamSang <sangnguyen12cb7@gmail.com>
2021-04-16 17:37:54 +0700

    crawl trang chu vnexpress

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb(); 
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = web.Load("https://vnexpress.net/du-lich");
+        }
+    }
+}
